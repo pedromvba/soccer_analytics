88fc54ec20dc1d61e2ad416e2868c9b81dc92f0d
pedromvba <pedromvba@gmail.com>
2024-10-02 15:23:10 -0300

    final code

---
+++ README.md
@@ -0,0 +1,31 @@
+# Análise de Jogos de Futebol
+
+Este projeto utiliza Streamlit para criar uma aplicação web interativa que traz dados de jogos de futebol para que sejam realizadas análises e exportação de determinada pelo Usuário.
+
+## Uso
+
+Para iniciar a aplicação, execute o seguinte comando:
+```bash
+streamlit run app/app.py
+```
+
+## Estrutura do Projeto
+
+- `app.py`: Arquivo principal da aplicação Streamlit.
+- `requirements.txt`: Lista de dependências do projeto.
+
+## Github do Projeto
+
+https://github.com/pedromvba/soccer_analytics
+
+## Contribuição
+
+1. Faça um fork do projeto.
+2. Crie uma branch para sua feature (`git checkout -b feature/nova-feature`).
+3. Commit suas alterações (`git commit -m 'Adiciona nova feature'`).
+4. Faça o push para a branch (`git push origin feature/nova-feature`).
+5. Abra um Pull Request.
+
+## Contato de Desenvolvedor
+
+pedromvba@gmail.com
--- app/app.py
+++ app/app.py
@@ -9,13 +9,10 @@ import json
 
 parser = Sbopen()
 
-
-
-
+# Functions
 
 def match_data(match_id):
     return parser.event(match_id)[0]
-
 
 def get_match_label(matches, match_id):
     row = matches[matches["match_id"] == match_id].iloc[0]
@@ -53,9 +50,7 @@ def plot_passes(match, player_name):
                   cmap='coolwarm',
                   alpha=0.5,
                   shade=True)
-
     return fig
-
 
 def plot_shots(match, player_name):
     player_filter = (match['type_name'] == 'Shot') & (match['player_name'] == player_name)
@@ -115,70 +110,92 @@ def pie_chart(label1, label2, value1, value2, title):
 def pandas_to_csv(df):
     return df.to_csv(index=False)
 
-
+# Main Function = App
 def main():
 
+    # initializing session state to track user parameters
+    if 'user_name' not in st.session_state:
+        st.session_state['user_name'] = ''
+
+    if 'competition' not in st.session_state:
+        st.session_state['competition'] = ''
+
+    if 'season' not in st.session_state:
+        st.session_state['season'] = ''
+
+    if 'game' not in st.session_state:
+        st.session_state['game'] = ''
+
+########## MENU ####################
     st.title("Análise de Jogos de Futebol")
 
     menu = ['Home', 'Explorar Dados dos Jogos', 'Cristiano Ronaldo']
     choice = st.sidebar.selectbox('Menu', menu)
 
+############ HOME PAGE ############################
     if  choice == 'Home':
         st.subheader('Bem vindo ao App de Análise de Jogos de Futebol')
         st.image('https://media.istockphoto.com/id/464558990/pt/foto/estádio-à-noite.jpg?s=612x612&w=0&k=20&c=gEjM94ZLSBuWOHOsarq-AV_F4cxjkkWfDxykZxfTDFU=')
         st.write('O objetivo do app é permitir a exploração de dados de jogos de futebol de diversas competições e temporadas')
-        st.write('Caso tenha alguma sugestão, favor entrar em contato com o desenvolvedor: pedromonteirovieira@gmail.com')
+        st.write('Caso tenha alguma sugestão, favor entrar em contato com o desenvolvedor: pedromvba@gmail.com')
 
-        # initializing session state to track user parameters
-
+        # User Name
         name = st.text_input('Digite seu Nome de Usuário')
         if name:
-            if 'user_name' not in st.session_state:
-                st.session_state['user_name'] = name
-                st.write('Usuário Salvo, favor usar o Menu para Navegar')
+            st.session_state['user_name'] = name
+            st.write('Usuário Salvo, favor usar o Menu para Navegar')
 
+        
+################ DATA EXPLORE PAGE ############################
     elif  choice == 'Explorar Dados dos Jogos':
         st.subheader('Explore aqui dados das partidas a partir das competições e temporadas disponíveis')
         st.image('images/soccer_analysis.jpg')
 
+        # Competitions
         competitions = sb.competitions()
         competitions_names = competitions['competition_name'].unique()
         competition = st.selectbox('Select uma Competição', competitions_names)
+        st.session_state['competition'] = competition
         competition_id = competitions[competitions["competition_name"] == competition]["competition_id"].values[0]
 
+        # Seasons
         seasons = competitions[competitions["competition_name"] == competition]["season_name"].unique()
         season_name = st. selectbox("Select uma Temporada", seasons)
         st.session_state['season'] = season_name
         season_id = competitions[competitions["season_name"] == season_name]["season_id"].values[0]
         
+        # Game
         matches = sb.matches(competition_id = competition_id, season_id=season_id)
-
-        game = st.selectbox("Select uma Partida", matches['match_id'],
+        game = st.selectbox("Select uma Partida", 
+                            matches['match_id'],
                             format_func= lambda idx:  get_match_label(matches, idx))
         st.session_state['game'] = game
-        st.write(st.session_state)
-    
 
-
+        # Match Details
         with st.container():
             st.markdown("<h1 style='text-align: center; color: blue; '>Match Details</h1>", unsafe_allow_html=True)
             
+            # Match Date
             date = matches[matches["match_id"] == game]["match_date"].values[0]
             st.markdown(f"<h5 style='text-align: center; color: blue; '>{date}</h5>", unsafe_allow_html=True)
             
+            # Referee
             referee = matches[matches["match_id"] == game]["referee"].values[0]
             st.markdown(f"<h6 style='text-align: center; color: blue; '>{referee}</h6>",unsafe_allow_html=True)
 
+            # Goals
             game_shots = sb.events(match_id=game, split=True, flatten_attrs=True)['shots']
             goal_shots = game_shots[game_shots['shot_outcome'] == 'Goal']
             for _, row in goal_shots.iterrows():
                 goal_data = f"Gol {row['team']} - {row['minute']} min - {row['player']}"
-                st.markdown(f"<h6 style='text-align: center; color: red; '>{goal_data}</h6>",unsafe_allow_html=True)
+                st.markdown(f"<p style='text-align: center; color: red; '>{goal_data}</p>",unsafe_allow_html=True)
 
 
         left_column, right_column = st.columns(2)
         
+        
         with st.container():
+            # Home Team
             with left_column:
                 st.subheader("Home Team")
                 home_team = matches[matches["match_id"] == game]["home_team"].values[0]
@@ -192,7 +209,7 @@ def main():
                 st.write(f"Chutes no Alvo: {home_shots_on_target} / {home_all_shots} ({home_shots_on_target_pct:.2%})")
                 
                 home_score = matches[matches["match_id"] == game]["home_score"].values[0]
-                st.metric("Goals", home_score)
+                st.metric("Gols", home_score)
 
 
                 st.write('Jogadores Disponíveis')
@@ -204,7 +221,7 @@ def main():
                 st.dataframe(home_lineup, hide_index=True)
 
 
-             
+            # Away Team 
             with right_column:
                 st.subheader ("Away Team")
                 away_team = matches[matches["match_id"] == game]["away_team"].values[0]
@@ -217,7 +234,7 @@ def main():
                 st.write(f"Chutes no Alvo: {away_shots_on_target} / {away_all_shots} ({away_shots_on_target_pct:.2%})")
 
                 away_score = matches[matches["match_id"] == game]["away_score"].values[0]
-                st.metric("Goals", away_score)
+                st.metric("Gols", away_score)
 
                 st.write('Jogadores Disponíveis')
                 lineups = sb.lineups(match_id=game)
@@ -227,7 +244,7 @@ def main():
                 away_lineup = away_lineup.rename(columns={'player_nickname': 'Jogador', 'jersey_number': 'Número'})
                 st.dataframe(away_lineup, hide_index=True)
 
-
+        # Events
         st.write('#### Eventos de Passes e Chutes da Partida por Jogador')
 
         home_names = home_lineup['Jogador'].unique()
@@ -251,69 +268,52 @@ def main():
 
             selected_events = shots_passes[shots_passes['player'].isin(players) & (shots_passes['minute'] >= time_box[0]) & (shots_passes['minute'] <= time_box[1])]
 
-            st.dataframe(selected_events.sort_values('minute'))
+            if selected_events.empty:
+                st.write('Nenhum Evento Encontrado')
+            else:
+                st.dataframe(selected_events.sort_values('minute'), hide_index=True)
             
-
-            st.write('Download dos Dados')
-            
-            st.download_button(
-                label='Clique para Baixar os Dados Selecionados',
-                data=pandas_to_csv(selected_events),
-                file_name='eventos_selecionados.csv')
-            
-
-        
-        
+                # Data Donwload
+                st.write('Download dos Dados')
+                
+                st.download_button(
+                    label='Clique para Baixar os Dados Selecionados',
+                    data=pandas_to_csv(selected_events),
+                    file_name='eventos_selecionados.csv')
 
 
-
-        #st.dataframe(goal_shots)
-        #st.dataframe(game_shots)
-
-
-
-
+################### CRISTIANO RONALDO PAGE ############################
     else:
         st.subheader('Cristiano Ronaldo - Portugal x Espanha - Copa 2018')
-
         st.markdown(f"<h3 style='text-align: center; color: red; '>{'Portugal 3 x 3 Espanha'}</h3>", unsafe_allow_html=True)
-        #st.subheader('Portugal 3 x 3 Espanha')
-
         st.image('https://conteudo.imguol.com.br/c/copadomundo/2018/imagem/b4/2018/06/15/cristiano-ronaldo-comemora-gol-de-portugal-contra-a-espanha-1529086642745_v2_1920x1365.jpg')
 
+        # Filtering Game Shots Data
         game_shots = sb.events(match_id=7576, split=True, flatten_attrs=True)['shots']
         goal_shots = game_shots[(game_shots['shot_outcome'] == 'Goal') & (game_shots['player'] == 'Cristiano Ronaldo dos Santos Aveiro')]
 
         st.write(' #### Gols de Portugal:')
-                
+        # Goals     
         for _, row in goal_shots.iterrows():
             goal_data = f"Gol - {row['minute']} min - Cristiano Ronaldo"
             st.markdown(f"<p style='text-align: left; color: red; '>{goal_data}</p>",unsafe_allow_html=True)
 
-
-        fifa_wold_cup_18_matches = sb.matches(competition_id=43, season_id=3)
-
-        #port_spain = fifa_wold_cup_18_matches[fifa_wold_cup_18_matches['match_id'] == 7576].iloc[0]
-        # final_match_id = fifa_wold_cup_22_matches[fifa_wold_cup_22_matches['match_id'] == 3869685].iloc[0]
+        # Game Data
         port_spain_data = match_data(7576)
 
-
-
-        
+        # CR7 and Portugal stats
         cr7_shots_on_target = game_shots[(game_shots['shot_outcome'].isin(['Goal', 'Saved'])) & (game_shots['player'] == 'Cristiano Ronaldo dos Santos Aveiro')].shape[0]
         cr7_all_shots = game_shots[game_shots['player'] == 'Cristiano Ronaldo dos Santos Aveiro'].shape[0]
         portugal_all_shots = game_shots[(game_shots['shot_outcome'].isin(['Goal', 'Saved'])) & (game_shots['team'] == 'Portugal')].shape[0]
         
         cr7_shots_on_target_pct = cr7_shots_on_target / cr7_all_shots if cr7_all_shots > 0 else 0
-        
-
         cr7_portugal_shots_on_target_pct = cr7_shots_on_target / portugal_all_shots if portugal_all_shots > 0 else 0
-
 
         st.write('### Aproveitamento Cristiano Ronaldo em Chutes')
 
         col1, col2 = st.columns(2)
         with col1:
+                # CR7 Stats
                 st.write(" #### Aproveitamento Cristiano Ronaldo")
                 st.markdown(f"<p>Percentual de Chutes no Gol</p><p style='color: green; font-size: 40px;'>{100 * cr7_shots_on_target_pct:.2f}%</p>", unsafe_allow_html=True)
                 
@@ -323,11 +323,9 @@ def main():
                   value2 = cr7_all_shots - cr7_shots_on_target,
                   title = 'Chutes no Gol Cristiano Ronaldo')
                 st.pyplot(fig3)
-                
-
-            
-            
+                       
         with col2:
+                # CR7 x Portugal Stats
                 st.write(" #### Cristiano Ronaldo x Outros Jogadores")
                 st.markdown(f"<p>Percentual de CR7 nos chutes a gol de Portugal</p><p style='color: green; font-size: 40px;'>{100 * cr7_portugal_shots_on_target_pct:.2f}%</p>", unsafe_allow_html=True)
 
@@ -339,9 +337,7 @@ def main():
         
                 st.pyplot(fig4)
 
-
-
-
+        # CR7 Maps
         st.write(' ### Mapas de Cristiano Ronaldo')
 
         # progress bar
@@ -355,78 +351,26 @@ def main():
 
         time.sleep(0.3)
     
-
+        # Passes Map
         st.write('#### Mapa de Passes')
         fig1 = plot_passes(port_spain_data, 'Cristiano Ronaldo dos Santos Aveiro')
         st.pyplot(fig1)
 
+        # Shots Map
         st.write(' #### Mapa de Chutes a Gol')
         fig2 = plot_shots(port_spain_data, 'Cristiano Ronaldo dos Santos Aveiro')
         st.pyplot(fig2)
 
-
+        # Foul Map
         df, related, freeze, tactics = parser.event(7576)
-
-
         st.write(' #### Mapa das Faltas Recebidas por Cristiano Ronaldo')
         df = df[(df.player_name == 'Cristiano Ronaldo dos Santos Aveiro') & (df.type_name == 'Foul Won')].copy()
-
         pitch = Pitch()
         fig5, ax = pitch.draw(figsize=(8, 6))
         hull = pitch.convexhull(df.x, df.y)
         poly = pitch.polygon(hull, ax=ax, edgecolor='cornflowerblue', facecolor='cornflowerblue', alpha=0.3)
         scatter = pitch.scatter(df.x, df.y, ax=ax, edgecolor='black', facecolor='cornflowerblue')
         st.pyplot(fig5)
-
-
-
-
-        # st.write(fifa_wold_cup_18_matches)
-        # st.write(port_spain_data)
-        # st.write(game_shots)
-
-        # Cristiano Ronaldo dos Santos Aveiro - 5207
-
-
-        #final_events = sb.events(match_id=3869685, split=True, flatten_attrs=True)
-        #lineups = sb.lineups(match_id=3869685)
-
-
-        # col1, col2 = st.columns(2)
-        # with col1:
-        #     st.write('Lionel Messi')
-        #     fig1 = plot_passes(final_data, 'Lionel Andrés Messi Cuccittini')
-        #     st.pyplot(fig1)
-
-
-
-
-        # with col2:
-        #     st.write('Kylian Mbappé')
-        #     fig2 = plot_passes(final_data, 'Kylian Mbappé Lottin')
-        #     st.pyplot(fig2)
-
-        
-        #st.write(lineups['France'])
-
-        # Kylian Mbappé Lottin - 3009
-        # Lionel Andrés Messi Cuccittini - 5503
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
